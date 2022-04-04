5fd63e554369e5e023369ffa539126f6480ad3ce
hayato8810 <hayato8810_0610@yahoo.co.jp>
2022-04-04 14:38:09 +0900

    add UnionFind

---
+++ library/UnionFind.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+#include <math.h>
+#include <cmath>
+#include <string>
+#include <map>
+#include <set>
+#include <queue>
+#include <deque>
+#include <algorithm>
+#include <utility>
+using namespace std;
+
+class UnionFind
+{
+public:
+    vector<int> par;
+
+    UnionFind(int N) : par(N)
+    {
+        for (int i = 0; i < N; i++)
+            par[i] = i;
+    }
+
+    int root(int x)
+    {
+        if (par[x] == x)
+            return x;
+        return par[x] = root(par[x]);
+    }
+
+    void unite(int x, int y)
+    {
+        int rx = root(x);
+        int ry = root(y);
+        if (rx == ry)
+            return;
+        par[rx] = ry;
+    }
+
+    bool same(int x, int y)
+    {
+        int rx = root(x);
+        int ry = root(y);
+        return rx == ry;
+    }
+};
