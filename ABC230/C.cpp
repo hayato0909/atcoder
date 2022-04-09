862609d6280d3d92aff7e69bb05283b85b5c42f1
hayato8810 <hayato8810_0610@yahoo.co.jp>
2022-04-09 12:04:23 +0900

    add C

--- ABC230/C.cpp
+++ ABC230/C.cpp
@@ -29,17 +29,14 @@ int main() {
     high = min(min(q-a, s-b), high);
     for(ll k=low;k<=high;k++) {
         board[a+k-p][b+k-r] = true;
-        cout << a+k-p << " " << b+k-r << endl;
     }
-    cout << endl;
+
     low = max(1-a, b-n), high = min(n-a, b-1);
     low = max(low, max(p-a, b-s));
     high = min(min(q-a, b-r), high);
     for(ll k=low;k<=high;k++) {
         board[a+k-p][b-k-r] = true;
-        cout << a+k-p << " " << b-k-r << endl;
     }
-    cout << endl;
 
     for(ll i=0;i<=q-p;i++) {
         for(ll j=0;j<=s-r;j++) {
