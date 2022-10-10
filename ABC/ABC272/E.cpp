5fa831c3df39d42f99ba744da4d40fa91862ff97
hayato0909 <hayato8810_0610@yahoo.co.jp>
2022-10-10 17:33:47 +0900

    add

--- ABC/ABC272/E.cpp
+++ ABC/ABC272/E.cpp
@@ -10,16 +10,21 @@ int main() {
     for(int i=1;i<=n;i++) cin >> a[i];
     vector<int> v[m+1];
     for(int i=1;i<=n;i++) {
-        int l = max(0, -a[i]/i);
+        if(a[i] >= n) continue;
+        int l;
+        if(a[i] >= 0) l = 1;
+        else l = (-a[i] + i - 1) / i;
+        int r = min(m + 1, (n - a[i] + i - 1) / i);
+        for(int j=l;j<r;j++) v[j].push_back(a[i] + j * i);
     }
 
     for(int i=1;i<=m;i++) {
-        bool exist[n+1] = {};
-        for(int j:v[i]) {
-            exist[j] = true;
-        }
+        set<int> s;
+        for(int j:v[i]) s.insert(j);
         int ans = 0;
-        while(exist[ans]) ans++;
+        for(ans = 0;ans <= n;ans++) {
+            if(!s.count(ans)) break;
+        }
         cout << ans << endl;
     }
 }
