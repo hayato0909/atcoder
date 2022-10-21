d6ccd4740539600b41415310b933e6049a030180
hayato0909 <hayato8810_0610@yahoo.co.jp>
2022-10-21 17:29:13 +0900

    add

---
+++ ABC/ABC218/A.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i, N, M) for(int i=N;i<M;i++)
+#define YES cout << "YES" << endl;
+#define NO cout << "NO" << endl;
+#define Yes cout << "Yes" << endl;
+#define No cout << "No" << endl;
+
+typedef long long ll;
+typedef pair<int, int> P;
+
+int main(){
+    int n; cin >> n;
+    string s; cin >> s;
+    if(s[n-1] == 'o') cout << "Yes" << endl;
+    else No;
+}
